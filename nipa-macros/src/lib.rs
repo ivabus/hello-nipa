5dd8a4cb23948cde4d29acb3df27862a6d86538b
Ivan Bushchik <ivabus@ivabus.dev>
2024-11-29 16:56:51 +0300

    Add _dot and _comma methods
    Signed-off-by: Ivan Bushchik <ivabus@ivabus.dev>

--- nipa-macros/src/lib.rs
+++ nipa-macros/src/lib.rs
@@ -32,6 +32,70 @@ pub fn letter(input: TokenStream) -> TokenStream {
             pub fn println_lower() {
                 println!("{}", stringify!(#ident).to_lowercase());
             }
+
+            pub fn print_upper_dot() {
+                print!("{}.", stringify!(#ident).to_uppercase());
+            }
+
+            pub fn print_lower_dot() {
+                print!("{}.", stringify!(#ident).to_lowercase());
+            }
+
+            pub fn print_upper_space_dot() {
+                print!("{}. ", stringify!(#ident).to_uppercase());
+            }
+
+            pub fn print_lower_space_dot() {
+                print!("{}. ", stringify!(#ident).to_lowercase());
+            }
+
+            pub fn print_upper_space_dot_after() {
+                print!("{} .", stringify!(#ident).to_uppercase());
+            }
+
+            pub fn print_lower_space_dot_after() {
+                print!("{} .", stringify!(#ident).to_lowercase());
+            }
+
+            pub fn println_upper_dot() {
+                println!("{}.", stringify!(#ident).to_uppercase());
+            }
+
+            pub fn println_lower_dot() {
+                println!("{}.", stringify!(#ident).to_lowercase());
+            }
+
+            pub fn print_upper_comma() {
+                print!("{},", stringify!(#ident).to_uppercase());
+            }
+
+            pub fn print_lower_comma() {
+                print!("{},", stringify!(#ident).to_lowercase());
+            }
+
+            pub fn print_upper_space_comma() {
+                print!("{}, ", stringify!(#ident).to_uppercase());
+            }
+
+            pub fn print_lower_space_comma() {
+                print!("{}, ", stringify!(#ident).to_lowercase());
+            }
+
+            pub fn print_upper_space_comma_after() {
+                print!("{} ,", stringify!(#ident).to_uppercase());
+            }
+
+            pub fn print_lower_space_comma_after() {
+                print!("{} ,", stringify!(#ident).to_lowercase());
+            }
+
+            pub fn println_upper_comma() {
+                println!("{},", stringify!(#ident).to_uppercase());
+            }
+
+            pub fn println_lower_comma() {
+                println!("{},", stringify!(#ident).to_lowercase());
+            }
         }
     })
 }
